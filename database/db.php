5eb8a22c6fa8869f3120fb7592763c8bd5974493
MarioSantoro <mariosantoro124@gmail.com>
2023-07-14 17:08:46 +0200

    Complete the exercise

--- database/db.php
+++ database/db.php
@@ -5,17 +5,23 @@ include_once __DIR__ . '/../models/Gioco.php';
 include_once __DIR__ . '/../models/Cuccia.php';
 include_once __DIR__ . '/../models/Cibo.php';
 
-$products = [
-    new Gioco(true, 'giallo', 'pupazzo', 10.99, '', new Categorie('Cane')),
-    new Gioco(false, 'rosso', 'osso', 6.99, '', new Categorie('Cane')),
-    new Gioco(true, 'viola', 'palla di lana', 15.99, '', new Categorie('Gatto')),
-    new Gioco(false, 'blu', 'sonaglio', 4.99, '', new Categorie('Gatto')),
-    new Cibo('Carne', 1, 'cibo per cani', 12.99, '', new Categorie('Cane')),
-    new Cibo('Pesce', 1, 'cibo per gatti', 16.99, '', new Categorie('Gatti')),
-    new Cibo('Manzo', 1, 'cibo per cani', 7.99, '', new Categorie('Cane')),
-    new Cibo('Agnello', 1, 'cibo per gatti', 29.99, '', new Categorie('Gatti')),
-    new Cuccia('Media', 'Media', 'Cuccia per cani nobili', 40.99, '', new Categorie('Cane')),
-    new Cuccia('Piccola', 'Piccola', 'Cuccia per cani plebei', 5.99, '', new Categorie('Cane')),
-    new Cuccia('Grande', 'Grande', 'Cuccia per gatti meastosi', 45.99, '', new Categorie('Gatto')),
-    new Cuccia('Media', 'Media', 'Cuccia per gatti nobili', 40.99, '', new Categorie('Gatto')),
+$game = [
+    new Gioco(true, 'giallo', 'pupazzo', 10.99, 'https://source.unsplash.com/random/250x250?sig=' . rand(0, 100), new Categorie('Cane')),
+    new Gioco(false, 'rosso', 'osso', 6.99,  'https://source.unsplash.com/random/250x250?sig=' . rand(0, 100), new Categorie('Cane')),
+    new Gioco(true, 'viola', 'palla di lana', 15.99,  'https://source.unsplash.com/random/250x250?sig=' . rand(0, 100), new Categorie('Gatto')),
+    new Gioco(false, 'blu', 'sonaglio', 4.99,  'https://source.unsplash.com/random/250x250?sig=' . rand(0, 100), new Categorie('Gatto')),
+];
+
+$food = [
+    new Cibo('Carne', 1, 'cibo per cani', 12.99,  'https://source.unsplash.com/random/250x250?sig=' . rand(0, 100), new Categorie('Cane')),
+    new Cibo('Pesce', 1, 'cibo per gatti', 16.99,  'https://source.unsplash.com/random/250x250?sig=' . rand(0, 100), new Categorie('Gatti')),
+    new Cibo('Manzo', 1, 'cibo per cani', 7.99,  'https://source.unsplash.com/random/250x250?sig=' . rand(0, 100), new Categorie('Cane')),
+    new Cibo('Agnello', 1, 'cibo per gatti', 29.99,  'https://source.unsplash.com/random/250x250?sig=' . rand(0, 100), new Categorie('Gatti')),
+];
+
+$cuccia = [
+    new Cuccia('Media', 'Media', 'Cuccia per cani nobili', 40.99,  'https://source.unsplash.com/random/250x250?sig=' . rand(0, 100), new Categorie('Cane')),
+    new Cuccia('Piccola', 'Piccola', 'Cuccia per cani plebei', 5.99,  'https://source.unsplash.com/random/250x250?sig=' . rand(0, 100), new Categorie('Cane')),
+    new Cuccia('Grande', 'Grande', 'Cuccia per gatti meastosi', 45.99,  'https://source.unsplash.com/random/250x250?sig=' . rand(0, 100), new Categorie('Gatto')),
+    new Cuccia('Media', 'Media', 'Cuccia per gatti nobili', 40.99,  'https://source.unsplash.com/random/250x250?sig=' . rand(0, 100), new Categorie('Gatto')),
 ];
--- index.php
+++ index.php
@@ -24,9 +24,104 @@ include_once __DIR__ . '/database/db.php';
         </div>
     </header>
     <main>
-        <div class="container">
+        <div class="container mt-4">
             <div class="row">
-                <?php foreach ($products as $item) { ?>
+                <?php foreach ($game as $item) { ?>
+                    <div class="col-3 mb-4">
+                        <div class="card">
+                            <img src="<?php echo $item->image ?>" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $item->name ?></h5>
+                                <p class="card-text">
+                                    Prezzo :
+                                    <?php echo $item->price . '€' ?>
+                                </p>
+                                <p class="card-text">
+                                    Morbido :
+                                    <?php if ($item->morbido) {
+                                        echo 'SI';
+                                    } else {
+                                        echo 'NO';
+                                    } ?>
+                                </p>
+                                <p class="card-text">
+                                    Colore :
+                                    <?php echo $item->colore ?>
+                                </p>
+                                <p class="card-text">
+                                    Animale :
+                                    <?php echo $item->categoria->categoria ?>
+                                </p>
+                                <a href="#" class="btn btn-primary">Acquista</a>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+
+                <?php foreach ($food as $item) { ?>
+                    <div class="col-3  mb-4">
+                        <div class="card">
+                            <img src="<?php echo $item->image ?>" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $item->name ?></h5>
+                                <p class="card-text">
+                                    Prezzo :
+                                    <?php echo $item->price . '€' ?>
+                                </p>
+                                <p class="card-text">
+                                    Tipo :
+                                    <?php echo $item->tipo ?>
+                                </p>
+                                <p class="card-text">
+                                    Peso :
+                                    <?php echo $item->peso . 'Kg' ?>
+                                </p>
+                                <p class="card-text">
+                                    Animale :
+                                    <?php echo $item->categoria->categoria ?>
+                                </p>
+                                <a href="#" class="btn btn-primary">Acquista</a>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+
+                <?php foreach ($cuccia as $item) { ?>
+                    <div class="col-3">
+                        <div class="card">
+                            <img src="<?php echo $item->image ?>" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $item->name ?></h5>
+                                <p class="card-text">
+                                    Prezzo :
+                                    <?php echo $item->price . '€' ?>
+                                </p>
+                                <p class="card-text">
+                                    Grandezza :
+                                    <?php echo $item->grandezza ?>
+                                </p>
+                                <p class="card-text">
+                                    Taglia Animale :
+                                    <?php echo $item->taglia_animale ?>
+                                </p>
+                                <p class="card-text">
+                                    Animale :
+                                    <?php echo $item->categoria->categoria ?>
+                                </p>
+                                <a href="#" class="btn btn-primary">Acquista</a>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+        </div>
+    </main>
+</body>
+
+</html>
+
+<!-- <?php foreach ($products as $item) { ?>
                     <div class="col-3">
                         <div class="card">
                             <img src="..." class="card-img-top" alt="...">
@@ -42,11 +137,4 @@ include_once __DIR__ . '/database/db.php';
                             </div>
                         </div>
                     </div>
-                <?php } ?>
-            </div>
-        </div>
-        </div>
-    </main>
-</body>
-
-</html>
+                <?php } ?> -->
